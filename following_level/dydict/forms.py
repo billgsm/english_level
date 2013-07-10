436d0808b2a8cd45fe031aaed2e8214ce7de8aab
bilou <ga_bilou@hotmail.com>
2013-07-10 12:02:33 +0200

    increase the number of fields

--- following_level/dydict/forms.py
+++ following_level/dydict/forms.py
@@ -8,9 +8,15 @@ class WordForm(forms.Form):
     """Word fields"""
 
     word = forms.CharField(max_length=50,
-                           widget=forms.TextInput(attrs={'placeholder': 'word'}))
-    definition = forms.CharField(widget=forms.Textarea(attrs={'class': 'input-xlarge', 'rows': '8', 'placeholder': 'definitions, examples ...'}))
-#Should figure out why clean_word doesn't work and show up a syntax error o_O
+        widget=forms.TextInput(attrs={'placeholder': 'The word', 'x-webkit-speech': True}))
+    definition = forms.CharField(max_length=500,
+        widget=forms.TextInput(attrs={'placeholder': 'Definition'}))
+    user_def = forms.CharField(max_length=500,
+        widget=forms.TextInput(attrs={'placeholder': 'My definition'}))
+    word_ref = forms.CharField(max_length=500,
+        widget=forms.TextInput(attrs={'placeholder': 'The source'}))
+    #definition = forms.CharField(widget=forms.Textarea(attrs={'class': 'input-xlarge', 'rows': '8', 'placeholder': 'definitions, examples ...'}))
+    #Should figure out why clean_word doesn't work and show up a syntax error o_O
     def clean(self):
         cleaned_data = super(WordForm, self).clean()
         exists = Dict.objects.filter(word=cleaned_data.get('word'))
@@ -51,5 +57,6 @@ class RegisterForm(forms.Form):
         return cleaned_data
 
 class LoginForm(forms.Form):
+    """Login fields"""
     username = forms.CharField(label="Login", max_length=30)
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
