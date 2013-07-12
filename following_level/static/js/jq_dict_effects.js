e9b7a88041bad62dd92291940edee9e97aa6518f
bilou <ga_bilou@hotmail.com>
2013-07-12 20:10:56 +0200

    no more js code into templates+adding bootstrap-paginator

---
+++ following_level/static/js/jq_dict_effects.js
@@ -0,0 +1,10 @@
+jQuery(function($) {
+  $("table#sortTable").tablesorter({ sortList: [[1,0]] });
+/*          $(".modal .close").bind("click", function(){
+      $(".modal-backdrop").fadeOut();
+      return false;
+  });*/
+  $('.carousel').carousel({
+    interval: 5000
+  })
+});
