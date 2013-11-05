698dec2f83ee7b1a016adcac0cc4cb6ad8dc81e4
bilou <ga_bilou@hotmail.com>
2013-11-05 20:16:30 +0100

    unittests integrated

--- following_level/dydict/tests.py
+++ following_level/dydict/tests.py
@@ -1,6 +1,34 @@
 #-*- coding: utf-8 -*-
 from django.test import TestCase
+from django.contrib.auth.models import User
 
+from dydict.models import Internaute, Dict
+
+
+class testInternaute(TestCase):
+  def test_create_user(self):
+    user = User(username="username",
+                password="password",
+                email="email@email.fr"
+           )
+    user.save()
+    internaute1 = Internaute(user=user)
+    internaute1.save()
+    # Check what has been saved
+    internaute = Internaute.objects.get(user__username="username")
+    self.assertEqual(internaute.user.email, "email@email.fr")
+
+  def test_create_dict(self):
+    self.test_create_user()
+    internaute = Internaute.objects.get(user__username='username')
+    word1 = Dict(word="my word",
+                definition="my definition",
+                user_def="my user_def",
+                word_ref="my word_ref",
+                internaute=internaute)
+    word1.save()
+    word = Dict(word="my word")
+    self.assertEqual(word.word_ref, u'my word_ref')
 
 class SimpleTest(TestCase):
     def test_dydict(self):
