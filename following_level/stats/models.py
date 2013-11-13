6ed47d213850558a4a720e58190db1102aa77951
bilou <ga_bilou@hotmail.com>
2013-11-13 13:24:41 +0100

    stats enhancements: visit numbers per page+ which ip known user is using

--- following_level/stats/models.py
+++ following_level/stats/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
-from usermanagement.models import Internaute
+from dydict.models import Internaute
+
 
 class Page(models.Model):
   url = models.URLField()
   visit_number = models.IntegerField(default=1)
-  #ip_address = models.IPAddressField()
+  see_page_date = models.DateTimeField(auto_now_add=True,
+                    verbose_name="The date when this page was visited")
+
+  def __unicode__(self):
+    return u'{0}'.format(self.url)
+
+
+class Visit(models.Model):
+  ip_address = models.IPAddressField()
   visit_date = models.DateTimeField(auto_now_add=True,
                                      auto_now=True,
                                      verbose_name="Visit date")
-  #user = models.OneToOneField(Internaute, blank=True)
+  internaute = models.ForeignKey(Internaute)
 
   def __unicode__(self):
-    return self.url
+    return u'{0}'.format(self.ip_address)
